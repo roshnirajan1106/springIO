49de2cda3b6bbc96e6d0151da48617e65abc80cc
roshnirajan1106 <roshnirajan8888@gmail.com>
2023-08-01 10:38:10 +0530

    made 6 packages

---
+++ src/test/java/com/db/application/DemoSpringProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
