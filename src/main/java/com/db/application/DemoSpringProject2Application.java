dbf378a48c328615886140efa5ca5bb3dc554d1d
roshnirajan1106 <roshnirajan8888@gmail.com>
2023-08-01 11:10:48 +0530

    added a new package

--- src/main/java/com/db/application/DemoSpringProject2Application.java
+++ src/main/java/com/db/application/DemoSpringProject2Application.java
@@ -2,12 +2,15 @@ package com.db.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class DemoSpringProject2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringProject2Application.class, args);
+		System.out.println("Helloo");
+		System.out.println("world");
 	}
 
 }
